1dff48d2bacb3a578151fadb4fbef47f7bb3e2d2
Your Name <your-email@example.com>
2025-08-15 19:54:33 +0530

    Final Changes

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -3,10 +3,12 @@ import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
 import { useAppContext } from '../../context/AppContext';
 import toast from 'react-hot-toast';
+import {parse} from 'marked';
 
 const AddBlog = () => {
     const {axios}=useAppContext();
     const [isAdding, setIsAdding]=useState(false);
+    const [loading, setLoading]=useState(false);
     const editorRef = useRef(null);
     const quillRef = useRef(null);
 
@@ -15,10 +17,24 @@ const AddBlog = () => {
     const [title, setTitle] = useState('');
     const [subTitle, setSubTitle] = useState('');
     const [category, setCategory] = useState('Startup');
-    const [isPublished, setIsPublished] = useState('false');
+    const [isPublished, setIsPublished] = useState(false);
 
     const generateContent = async () => {
-
+        if(!title) return toast.error('Please enter a title');
+        try {
+            setLoading(true);
+            const {data}= await axios.post('/api/blog/generate', {prompt: title});
+            if(data.success){
+                quillRef.current.root.innerHTML = parse(data.content);
+            }else{
+                toast.error(data.message);   
+            }
+        } catch (error) {
+            toast.error(error.message);
+            
+        }finally{
+            setLoading(false);
+        }
     }
 
     const onSubmitHandler = async (e) => {
@@ -36,7 +52,7 @@ const AddBlog = () => {
                 toast.success(data.message);
                 setImage(false);
                 setTitle('');
-                subTitle('');
+                setSubTitle('');
                 quillRef.current.root.innerHTML=''
                 setCategory('Startup')
             }else{
@@ -74,7 +90,10 @@ const AddBlog = () => {
                 <p className='mt-4'>Blog Description</p>
                 <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
                     <div ref={editorRef} ></div>
-                    <button className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer' type='button' onClick={generateContent}>Generate with AI</button>
+                    {loading && (<div className='absolute right-0 top-0 bottom-0 left-0 flex items-center justify-center bg-black/10 mt-2'>
+                    <div className='w-8 h-8 rounded-full border-2 border-t-white animate-spin'></div>
+                        </div>)}
+                    <button disabled={loading} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer' type='button' onClick={generateContent}>Generate with AI</button>
                 </div>
 
                 <p className='mt-4'>Blog Category</p>
--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,6 +2,7 @@ import fs from "fs"
 import imagekit from "../configs/imageKit.js";
 import Blog from "../models/Blog.js";
 import Comment from "../models/Comment.js";
+import main from "../configs/gemini.js";
 
 export const addBlog = async (req, res) => {
     try {
@@ -116,5 +117,18 @@ export const getBlogComments = async (req, res) => {
         res.json({ success: true, comments });
     } catch (error) {
         req.json({ success: false, message: error.message });
+    }
+}
+
+export const generateContent = async (req, res)=>{
+    try {
+        const {prompt} = req.body;
+        if (!prompt) {
+            return res.status(400).json({ success: false, message: 'Prompt not provided' });
+        }
+        const content = await main(prompt + ' Generate a blog content for this topic in simple text format');
+        res.json({success:true, content});
+    } catch (error) {
+        res.json({success:false, message:error.message});
     }
 }
